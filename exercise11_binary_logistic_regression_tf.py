2db842da0c75efb4469ab450e03f6c6344ba43a2
Frank Schultz <scf175@googlemail.com>
2022-01-16 17:08:51 +0100

    Update exercise11_binary_logistic_regression_tf.py

--- exercise11_binary_logistic_regression_tf.py
+++ exercise11_binary_logistic_regression_tf.py
@@ -12,7 +12,10 @@
 # cf. especially week 2
 # compare results against a TensorFlow model
 
+import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
 
@@ -20,6 +23,8 @@ print(tf.__version__)
 
 # rng = np.random.RandomState(1)  # for debug
 rng = np.random.RandomState()
+
+verbose = 1  # plot training status
 
 
 def my_sigmoid(z):
@@ -51,39 +56,38 @@ def evaluate(y_true, y_pred):
     return cm, n_cm, precision, recall, F1_score
 
 
-def create_data(nx, m, mean_offset):
-    # std=1,  all features have same +mean
-    X1 = rng.randn(nx, m//2) + mean_offset
-    # vary the variance [0...1] of the features
-    for i in range(nx):
-        X1[i, :] *= rng.rand(1)
-    # class 1 code
-    Y1 = np.ones((1, m//2))
-
-    X2 = rng.randn(nx, m//2) - mean_offset
-    for i in range(nx):
-        X2[i, :] *= rng.rand(1)
-        Y2 = np.zeros((1, m//2))
-
-    X = np.concatenate((X1, X2), axis=1)
-    Y = np.concatenate((Y1, Y2), axis=1)
-    return X, Y
-
-
 # gradient descent hyper parameters
 # set up to see how things evolve
 # in practice do automated hyper parameter search
-step_size = 0.3
-steps = 5000
+step_size = 0.25
+steps = 500
 
-# we create some training data
-mean_offset = 0.75  # apply +-mean_offset to randn data to simulate two classes
+# we create some data
 m = 100000  # training data examples, even int!
-m_train = m//10  # test data examples, even int!
-nx = 5  # number of features, increasing -> higher F1 score
-X, Y = create_data(nx, m, mean_offset)
+nx = 2  # number of features
+train_size = 0.8  # 80% are used for training
 
-# TRAIN
+X, Y = make_classification(n_samples=m,
+                           n_features=nx, n_informative=nx,
+                           n_redundant=0, n_classes=2,
+                           n_clusters_per_class=1,
+                           class_sep=1,
+                           flip_y=1e-2,
+                           random_state=8)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+                                                    train_size=train_size,
+                                                    random_state=None)
+m_train = X_train.shape[0]
+print('\nm_train', m_train)
+
+X = X_train.T  # our implementation needs transposed data
+Y = np.expand_dims(Y_train, axis=0)
+# X.shape = (nx, m_train)
+# Y.shape = (1, m_train)
+print('X train dim', X.shape, 'Y train dim', Y.shape)
+
+
+# TRAINING PHASE
 #     OUR MODEL
 # we init weights and bias
 w = (rng.rand(nx, 1) - 1/2) * 2
@@ -96,14 +100,16 @@ for step in range(steps):
     # using the current weights and bias:
     Z = np.dot(w.T, X) + b  # forward step 1 = inner product + bias
     A = my_sigmoid(Z)  # forward step 2 = activation function
-    print('epoch', step, '/', steps, ', our cost on training data', cost(Y, A))
+    if verbose:
+        print('epoch', step, '/', steps, ', our cost on training data',
+              cost(Y, A))
 
     # backward propagation, start at the output from model and subsequently
     # move back to the model input
     # vectorized
     da = -Y / A + (1-Y) / (1-A)  # step1 -> dL/da
     dz = da * A*(1-A)  # step 2 -> (dL/da) * da/dz
-    dw = np.dot(X, dz.T) / m  # step 3 -> dL/dw = (dL/da * da/dz) * dz/dw
+    dw = np.dot(X, dz.T) / m_train  # step 3 -> dL/dw = (dL/da * da/dz) * dz/dw
     db = np.mean(dz)  # dL/dw = dL/da * da/dz * dz/db
 
     # gradient descent update rule
@@ -132,10 +138,11 @@ output = tf.keras.layers.Dense(1, kernel_initializer=initializer,
 # output = tf.keras.layers.Dense(1, activation=tf.math.sigmoid)(input)
 model = tf.keras.Model(inputs=input, outputs=output)
 model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-model.fit(X.T, Y.T, batch_size=m, epochs=steps, verbose=1)
+model.fit(X.T, Y.T, batch_size=m_train, epochs=steps, verbose=verbose)
 # explicit usage of epochs, batch_size (here hard coded):
-# model.fit(X.T, Y.T, epochs=20, batch_size=100, verbose=1)
-results_train_tf = model.evaluate(X.T, Y.T, batch_size=m, verbose=0)
+# model.fit(X.T, Y.T, epochs=20, batch_size=100, verbose=verbose)
+results_train_tf = model.evaluate(X.T, Y.T, batch_size=m_train,
+                                  verbose=verbose)
 
 # print results of our model / TF model
 print(model.summary())
@@ -155,11 +162,15 @@ print('ours\nw', w.T,
 print('TF\nw', model.get_weights()[0].T,
       '\nb', model.get_weights()[1])
 
-# TEST
+# TESTING PHASE
 # we check model performance on !! unseen !! test data
-# therefore we create some test data with same
-# PDF characteristics as training data
-X, Y = create_data(nx, m_train, mean_offset)
+m_test = X_test.shape[0]
+print('\nm_test', m_test)
+X = X_test.T  # our implementation needs transposed data
+Y = np.expand_dims(Y_test, axis=0)
+# X.shape = (nx, m_test)
+# Y.shape = (1, m_test)
+print('X test dim', X.shape, 'Y test dim', Y.shape)
 
 #     OUR MODEL
 # do model prediction == forward propagation using test data
@@ -169,7 +180,7 @@ _, n_cm_test, precision_test, recall_test, F1_score_test =\
     evaluate(np.copy(Y), np.copy(A))
 
 #     TensorFlow MODEL
-results_test_tf = model.evaluate(X.T, Y.T, batch_size=m_train, verbose=0)
+results_test_tf = model.evaluate(X.T, Y.T, batch_size=m_test, verbose=verbose)
 
 # print results of our model / TF model
 print('\n\nmetrics on test data:')
@@ -182,3 +193,32 @@ print('TF recall ', results_test_tf[4])
 print('our confusion matrix\n[TP FP]\n[FN TN] =\n',
       n_cm_test*100, '%')
 print('our F1_score', F1_score_test*100, '%')
+
+if nx == 2:  # 2D plot of data and classification line
+    x = np.arange(-6, 6, 0.1)
+    y = np.arange(-6, 6, 0.1)
+    xv, yv = np.meshgrid(x, y)
+    tmp = my_sigmoid(w[0]*xv + w[1]*yv + b)
+    tmp[tmp > 0.5] = 1
+    tmp[tmp <= 0.5] = 0
+
+    plt.figure(figsize=(10, 10))
+    plt.subplot(2, 1, 1)
+    plt.plot(X_train[Y_train == 0, 0], X_train[Y_train == 0, 1], 'C0o', ms=1)
+    plt.plot(X_train[Y_train == 1, 0], X_train[Y_train == 1, 1], 'C1o', ms=1)
+    plt.contourf(x, y, tmp, cmap='RdBu_r')
+    plt.axis('equal')
+    plt.colorbar()
+    plt.title(X_train.shape)
+    plt.xlabel('feature 1')
+    plt.ylabel('feature 2')
+    plt.subplot(2, 1, 2)
+    plt.plot(X_test[Y_test == 0, 0], X_test[Y_test == 0, 1], 'C0o', ms=1)
+    plt.plot(X_test[Y_test == 1, 0], X_test[Y_test == 1, 1], 'C1o', ms=1)
+    plt.contourf(x, y, tmp, cmap='RdBu_r')
+    plt.axis('equal')
+    plt.colorbar()
+    plt.title(X_test.shape)
+    plt.xlabel('feature 1')
+    plt.ylabel('feature 2')
+    plt.savefig('exercise11_binary_logistic_regression_tf.png')
