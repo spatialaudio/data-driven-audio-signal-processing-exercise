e08a32ef184b28f20b3a401d763171ff8ddb5f75
Frank Schultz <scf175@googlemail.com>
2025-10-18 14:00:57 +0200

    remove outputs, linting

--- util_binary_logistic_regression.py
+++ util_binary_logistic_regression.py
@@ -2,9 +2,12 @@ from numpy import exp
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 
+
 def toy_data(M=10000, train_size=0.8):
-    # M number of samples per feature
-    #train_size = 0.8  # 80% of data are used for training, 20% for testing
+    """Create toy data for binary logistic regression
+    M number of samples per feature
+    train_size = 0.8  # 80% of data are used for training, 20% for testing
+    """
     N = 2  # number of features (excluding bias)
     random_state = 12345
     X, Y = make_classification(
@@ -23,16 +26,21 @@ def toy_data(M=10000, train_size=0.8):
     )
     return X_train, Y_train, X_test, Y_test
 
+
 def init_weights():
-    # nice numbers here, in practice these
-    # are appropriately randomly sampled numbers
+    """Weights for the binary logistic regression, i.e. one sigmoid layer
+    nice numbers here, in practice these
+    are appropriately randomly sampled numbers
+    """
     w1 = 0.5
     w2 = 0.25
     b = 0.125
     return w1, w2, b
 
+
 def my_sigmoid(z):
     return 1.0 / (1.0 + exp(-z))
 
+
 def predict_class(y):
-    return (y >=0.5) * 1
+    return (y >= 0.5) * 1
